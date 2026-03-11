64d15a98939c117ee0447d155f82111e353768c8
jburkmier <121325890+jburkmier@users.noreply.github.com>
2026-03-10 23:19:33 -0400

    Changed structure from Browse to Recipe Book

--- app/src/sp/RecipeBook.java
+++ app/src/sp/RecipeBook.java
@@ -6,12 +6,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class Browse {
+public class RecipeBook {
 
     private final Connection connection;
     private final Scanner scanner;
 
-    public Browse(Scanner scanner, Connection connection) {
+    public RecipeBook(Scanner scanner, Connection connection) {
         this.scanner = scanner;
         this.connection = connection;
     }
@@ -19,16 +19,16 @@ public class Browse {
     public void run() {
         System.out.println("+++++ Browse Recipes +++++");
         System.out.println("1. View all recipes");
+        System.out.println("2. View Recipe Card");
         System.out.println("2. Exit");
 
         int choice = scanner.nextInt();
-        scanner.nextLine(); // Consume newline
+        scanner.nextLine();
 
-        if (choice == 2) {
+        if (choice == 3) {
             System.out.println("Exiting Browse...");
             return;
         }
-
         browseRecipes(choice);
     }
 
@@ -39,15 +39,11 @@ public class Browse {
         try {
             switch (choice) {
                 case 1 -> {
-                    System.out.println("\nRecipes: ");
-                    ps = connection.prepareStatement("""
-                        SELECT Recipe.Meal_name
-                        FROM RECIPE
-                    """);
-                    rs = ps.executeQuery();
-                    while (rs.next()) {
-                        System.out.println(rs.getString("Meal_name"));
+                    viewAll();
                     }
+                case 2 -> {
+                    System.out.println("\nView Recipe Card: ");
+                    System.out.println("Enter Name of Recipe to View Card");
                 }
                 default -> System.out.println("Invalid choice");
             }
@@ -62,4 +58,20 @@ public class Browse {
             }
         }
     }
+
+    public void viewAll(){                                                      //LEFT OFF HERE, FIX THIS, SWITCH TO INPROGRESS
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        System.out.println("\nRecipes: ");
+        ps = connection.prepareStatement("""
+            SELECT Recipe.Meal_name
+            FROM RECIPE
+            """);
+        rs = ps.executeQuery();
+        while (rs.next()) {
+        System.out.println(rs.getString("Meal_name"));
+
+    }
+}
 }
